9998aed90eee5ddaec50f680555dcbe236d75ad2
Ysaac Andujar <ysaacandujar@gmail.com>
2023-07-18 14:07:42 -0400

    notes appbar

--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import { NothingSelected } from './NothingSelected'
+import { NotesScreen } from 'components/notes/NotesScreen'
 
 export const JournalScreen = () => {
   return (
     <div className='journal__main-content'>
       <Sidebar />
       <main>
-        <NothingSelected/>
+        {/* <NothingSelected/> */}
+        <NotesScreen/>
       </main>
     </div>
   )
---
+++ src/components/notes/NotesAppBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+  return (
+    <div className='notes__appbar'>
+        <span>28 ads 123</span>
+        <div>
+            <button className='btn'>
+                Picture
+            </button>
+            <button className='btn'>
+                Save
+            </button>
+        </div>
+    </div>
+  )
+}
