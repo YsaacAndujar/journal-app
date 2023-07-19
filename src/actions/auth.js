374a9b07dcb6ea7e73056b7dc3280e6c6df40252
Ysaac Andujar <ysaacandujar@gmail.com>
2023-07-19 15:35:05 -0400

    firebase and google

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,18 @@
 import { types } from "types/types";
-
+import { firebase, googleAuthProvider } from '../firebase/firebaseConfig'
 export const startLoginEmailPassword = (email, password) =>{
     return (dispatch) => {
         setTimeout(() => {
             dispatch(login(email, password))
         }, 3500);
     }
+}
+
+export const startGoogleLogin = () => {
+    return (dispatch) =>{
+        firebase.auth().signInWithPopup(googleAuthProvider)
+        .then(userCred => {})
+    } 
 }
 
 export const login = (uid, displayName) => {
--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import { useForm } from 'hooks/useForm'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { startLoginEmailPassword } from 'actions/auth';
+import { startGoogleLogin, startLoginEmailPassword } from 'actions/auth';
 import { store } from 'store/store'
 export const LoginScreen = () => {
   
@@ -15,6 +15,10 @@ export const LoginScreen = () => {
   const handleLogin = (e) =>{
     e.preventDefault()
     dispatch(startLoginEmailPassword(email, password))
+  }
+
+  const handleGoogleLogin = () =>{
+    dispatch(startGoogleLogin())
   }
 
   const { email, password } = values
@@ -52,6 +56,7 @@ export const LoginScreen = () => {
           </p>
           <div 
             className="google-btn"
+            onClick={handleGoogleLogin}
           >
             <div className="google-icon-wrapper">
                 <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
