aadbb1658a5ebda453f52468ad39f17fa99d9a37
Ysaac Andujar <ysaacandujar@gmail.com>
2023-08-04 14:27:39 -0400

    show note notescreen

--- src/components/notes/NotesScreen.jsx
+++ src/components/notes/NotesScreen.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { NotesAppBar } from './NotesAppBar'
-
+import { useSelector } from 'react-redux'
+import {store} from 'store/store'
+import { useForm } from 'hooks/useForm'
 export const NotesScreen = () => {
+    // @ts-ignore
+    const {active:note} = useSelector(state=> state.notes)
+    const { dispatch } = store
+    const {values, handleInputChange, reset} = useForm(note)
+    const {body, title} = values
+    const activeId = useRef(note.id)
+    useEffect(() =>{
+        if(note.id !== activeId.current){
+            reset(note)
+            activeId.current = note.id
+        }
+    }, [reset, note])
+
   return (
     <div className="notes__main-content">
         <NotesAppBar />
@@ -11,18 +26,26 @@ export const NotesScreen = () => {
                 placeholder='Title'
                 className='notes__title-input'
                 autoComplete='off'
+                name='title'
+                value={title}
             />
             <textarea
                 className="notes__textarea"
                 placeholder='What happened today'
+                name='body'
+                value={body}
             ></textarea>
-
-            <div className="notes__image">
-                <img 
-                    src="https://cdn-icons-png.flaticon.com/512/1088/1088537.png?w=826&t=st=1689692716~exp=1689693316~hmac=55f9a66894fa8b94daadf5c645b71d69bbac6c84fc9cb0ec48a6efe6a5b1bf9b"
-                    alt="Imagen"
-                />
-            </div>
+            {
+                (note.url) 
+                && (
+                    <div className="notes__image">
+                        <img 
+                            src="https://cdn-icons-png.flaticon.com/512/1088/1088537.png?w=826&t=st=1689692716~exp=1689693316~hmac=55f9a66894fa8b94daadf5c645b71d69bbac6c84fc9cb0ec48a6efe6a5b1bf9b"
+                            alt="Imagen"
+                        />
+                    </div>
+                )
+            }
         </div>
     </div>
   )
